ea086bd11b8514740fdfb88384aaea2ff0538b4a
GianbaBotassi <gianba89@gmail.com>
2023-05-29 20:21:37 +0200

    Refactoring with li

--- js/main.js
+++ js/main.js
@@ -3,23 +3,14 @@ const { createApp } = Vue
 createApp({
     data(){
         return{
-            mailArray: []
+            mailArray: []       //array per mail
         }
     }
-    // ,methods:{
-    //     ciclo(){
-    //         for(let i = 0; i<10;i++){
-    //             this.mailArray.push('ciao')
-    //             console.log(this.mailArray);
-    //         }
-    //     }
-    // }
     ,mounted(){
-        for (let index = 0; index < 10; index++) {
+        for (let index = 0; index < 10; index++) {   //ciclo richiesta api
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
             then((response) => {
-                console.log(response.data.response);
-                this.mailArray.push(response.data.response)
+                this.mailArray.push(response.data.response)  //pusho risposta in array
             })
             
         }
